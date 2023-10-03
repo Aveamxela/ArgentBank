493e95ed49e181cdea6bafb4fd06b3f11a5e08c4
Maeva <maeva-froger@outlook.fr>
2023-10-03 11:14:03 +0200

    structure SignInCard -pas de useState ou Store

--- argentbank/src/pages/SignIn/SignIn.jsx
+++ argentbank/src/pages/SignIn/SignIn.jsx
@@ -1,5 +1,15 @@
+import Footer from "../../components/Footer/Footer";
+import HeaderLogOut from "../../components/Header_logout/Header_logout";
+import SignInCard from "../../components/SignInCard/SignInCard";
+
 export default function SignIn() {
-    return(
-        <h1>test Sign In</h1>
-    )
+    return (
+        <div>
+            <HeaderLogOut />
+                <main class="main bg-dark">
+                    <SignInCard/>
+                </main>
+            <Footer/>
+        </div>
+    );
 }
